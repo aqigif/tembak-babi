9f429e509271906619cf0433803a469dab39bc30
Aqil Gifari <aqilgifari0@gmail.com>
2022-04-07 09:58:34 +0700

    unity test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl: "/assets/tembak-babi-dev/Build/tembak-babi-dev.loader.js",
+  dataUrl: "/assets/tembak-babi-dev/Build/tembak-babi-dev.data",
+  frameworkUrl: "/assets/tembak-babi-dev/Build/tembak-babi-dev.framework.js",
+  codeUrl: "/assets/tembak-babi-dev/Build/tembak-babi-dev.wasm",
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Unity unityContext={unityContext} />;
 }
 
-export default App;
+export default App
