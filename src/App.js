625519185953c486e7bc54921a20b953f2863ae4
Aqil Gifari <aqilgifari0@gmail.com>
2022-04-07 10:22:48 +0700

    prod assets

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
-const assetsName = "tembak-babi-dev"
 const productionMode = true;
+const assetsName = productionMode ? "tembak-babi" : "tembak-babi-dev";
 const unityContext = new UnityContext({
   loaderUrl: `/assets/${assetsName}/Build/${assetsName}.loader.js`,
-  dataUrl: `/assets/${assetsName}/Build/${assetsName}.data${productionMode ? ".gz" : ""}`,
-  frameworkUrl: `/assets/${assetsName}/Build/${assetsName}.framework.js${productionMode ? ".gz" : ""}`,
-  codeUrl: `/assets/${assetsName}/Build/${assetsName}.wasm${productionMode ? ".gz" : ""}`,
+  dataUrl: `/assets/${assetsName}/Build/${assetsName}.data${
+    productionMode ? ".gz" : ""
+  }`,
+  frameworkUrl: `/assets/${assetsName}/Build/${assetsName}.framework.js${
+    productionMode ? ".gz" : ""
+  }`,
+  codeUrl: `/assets/${assetsName}/Build/${assetsName}.wasm${
+    productionMode ? ".gz" : ""
+  }`,
 });
 
 function App() {
   return <Unity unityContext={unityContext} />;
 }
 
-export default App
+export default App;
