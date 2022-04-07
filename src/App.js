7c74bab10c9d52ab5cf473d50a6fd86716898f6a
Aqil Gifari <aqilgifari0@gmail.com>
2022-04-07 13:01:43 +0700

    fullscreen css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import PGShoot from "./pages/pig-shoot";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Meteor Unity!</h1>
       <Routes>
         <Route path="/" element={<PGShoot />} />
         <Route path="js-listen" element={<JSListen />} />
